b1b4809275a240ecad1761a1d46c9909034cd132
Venky Venkatakrishnan <eeshwar1@gmail.com>
2023-08-19 20:17:51 -0500

    Initial Commit

---
+++ VUCalendar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  VUCalendar
+//
+//  Created by Venkateswaran Venkatakrishnan on 8/19/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
